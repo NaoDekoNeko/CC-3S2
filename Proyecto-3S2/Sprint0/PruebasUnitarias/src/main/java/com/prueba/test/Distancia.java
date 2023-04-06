fd40c1ee97049141fb3affd53059a3d5b442d716
GabrielBarrientos99 <gabriel.barrientos.c@uni.pe>
2023-04-06 13:05:59 -0500

    inicio de pruebas unitarias-trabajando Ambos

---
+++ Proyecto-3S2/Sprint0/PruebasUnitarias/src/main/java/com/prueba/test/Distancia.java
@@ -0,0 +1,8 @@
+package com.prueba.test;
+
+public class Distancia {
+    // Función que calcula la distancia de un punto al Origen
+    public double calcularDistancia(Coordenada punto) {
+        return -1;
+    }
+}

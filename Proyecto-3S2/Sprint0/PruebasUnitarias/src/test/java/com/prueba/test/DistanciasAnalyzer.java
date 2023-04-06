d8b5e5e1c3816b0d216ae248bd8741769616209c
GabrielBarrientos99 <gabriel.barrientos.c@uni.pe>
2023-04-06 13:39:57 -0500

    UnitTest Terminado Exitosamente-Trabajamos con Code With me

--- Proyecto-3S2/Sprint0/PruebasUnitarias/src/test/java/com/prueba/test/DistanciasAnalyzer.java
+++ Proyecto-3S2/Sprint0/PruebasUnitarias/src/test/java/com/prueba/test/DistanciasAnalyzer.java
@@ -1,6 +1,9 @@
 package com.prueba.test;
 
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.closeTo;
 
 public class DistanciasAnalyzer {
 
@@ -9,6 +12,7 @@ public class DistanciasAnalyzer {
         var distancia = new Distancia();
         var punto = new Coordenada(0,0);
         var resultado = distancia.calcularDistancia(punto);
+        assertThat(resultado,equalTo(0.0));
     }
 
     @Test
@@ -16,29 +20,31 @@ public class DistanciasAnalyzer {
         var distancia = new Distancia();
         var punto = new Coordenada(3,4);
         var resultado = distancia.calcularDistancia(punto);
-
+        assertThat(resultado,equalTo(5.0));
     }
 
     @Test
     void SegundoCuadrante() {
         var distancia = new Distancia();
-        var punto = new Coordenada(-3,4);
+        var punto = new Coordenada(-6,8);
         var resultado = distancia.calcularDistancia(punto);
-        
+        assertThat(resultado,equalTo(10.0));
     }
 
     @Test
     void TercerCuadrante() {
         var distancia = new Distancia();
-        var punto = new Coordenada(-3,-4);
+        var punto = new Coordenada(-12,-5);
         var resultado = distancia.calcularDistancia(punto);
+        assertThat(resultado,equalTo(13.0));
     }
 
     @Test
     void CuartoCuadrante() {
         var distancia = new Distancia();
-        var punto = new Coordenada(3,-4);
+        var punto = new Coordenada(1,-1);
         var resultado = distancia.calcularDistancia(punto);
+        assertThat(resultado, closeTo(1.4142, 0.0001));
     }
 
 }

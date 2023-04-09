2b36bde647a5a0d95049b2e589367878fd590a34
NaoDekoNeko <ademarx17@gmail.com>
2023-04-09 18:23:39 -0500

    Se migró el proyecto de Java a C#, cambio de IDE y Avance de Sprint 1, Sprint 0 terminado, trabajamos ambos

--- Proyecto-3S2/Sprint0/PruebasUnitarias/src/test/java/com/prueba/test/DistanciasAnalyzer.java
+++ Proyecto-3S2/Sprint0/PruebasUnitarias/src/test/java/com/prueba/test/DistanciasAnalyzer.java
@@ -5,7 +5,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.closeTo;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 
 public class DistanciasAnalyzer {
 

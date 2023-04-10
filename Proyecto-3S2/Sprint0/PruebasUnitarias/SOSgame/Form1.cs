ff56bd7089b4593c4d1d164da3f75342a30a44e0
NaoDekoNeko <ademarx17@gmail.com>
2023-04-09 21:20:53 -0500

    Terminado Sprint0 en markdown

---
+++ Proyecto-3S2/Sprint0/PruebasUnitarias/SOSgame/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sprint0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //extraemos las coordenadas y las convertimos a double
+            double X1 = Convert.ToDouble(textBox1.Text);
+            double Y1 = Convert.ToDouble(textBox2.Text);
+
+            //instanciamos las clases  
+            var distancia = new Distancia();
+            var coordenadas = new Coordenada(X1, Y1);
+            var impri = new Imprimir();
+
+            //variable que guarda el resultado en double
+            var resultado = distancia.CalcularDistancia(coordenadas);
+            //decision de cómo se mostrará (decimales a usar)
+            //no se usó if inmediato (condition ? consecuente : alternativa)
+            if (checkBox1.Checked)
+                textBox5.Text = impri.Impresion(resultado, Convert.ToInt16(numericUpDown1.Value));
+            else
+                textBox5.Text = impri.Impresion(resultado);
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

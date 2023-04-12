c412520a7c7d82c3d257c82f265f5bf60a5698c9
NaoDekoNeko <ademarx17@gmail.com>
2023-04-11 21:38:03 -0500

    Apliqué principios SOLID, logré hacer el tablero dinámico pero falta que la asignación de letras también lo sea, lo siento por meternos de putazo a C# Gabo :'v

---
+++ Proyecto-3S2/Sprint2/SOSgame/SOSgame/Tamanio_Tablero.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
+
+namespace SOSgame
+{
+    public class Tamanio_Tablero:Form1
+    {
+        public void Asignar_Tamanio(int tamanio, TableLayoutPanel panel)
+        {
+            panel.ColumnCount = tamanio;
+            panel.RowCount = tamanio;
+            for (int i = 0; i < tamanio; i++)
+                panel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100 / tamanio));
+            for (int i = 0; i < tamanio; i++)
+                panel.RowStyles.Add(new RowStyle(SizeType.Percent, 100 / tamanio));
+            //MessageBox.Show("funciona i guess");
+        }
+    }
+}

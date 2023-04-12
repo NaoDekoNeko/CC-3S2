91f91b4623cab3e9faa8b16beffd2f899aa2e38a
NaoDekoNeko <ademarx17@gmail.com>
2023-04-12 08:29:18 -0500

    inicio TDD3

---
+++ Actividades/TDD-3/TDD-3/src/main/java/com/wordz/Wordz.java
@@ -0,0 +1,11 @@
+package com.wordz;
+
+public class Wordz {
+    public static void main(String[] args) {
+        // No implementado
+    }
+}
+
+/*
+Problema: Analiza y explica el código anterior. ¿Se requiere que cambiemos el código de prueba para reflejar ese cambio en la firma del método?. ¿hay algún riesgo en esto?.
+ */

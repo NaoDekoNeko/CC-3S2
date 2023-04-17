1c6f935c896281044ad7908d0cb3247cac750f40
NaoDekoNeko <ademarx17@gmail.com>
2023-04-17 12:05:00 -0500

    TDD3 terminada en clase

--- Actividades/TDD-3/TDD-3/src/test/java/com/wordz/domain/WordTest.java
+++ Actividades/TDD-3/TDD-3/src/test/java/com/wordz/domain/WordTest.java
@@ -9,19 +9,36 @@ public class WordTest {
     @Test
     public void oneIncorrectLetter() {
         var word = new Word("A");
+        var score = word.guess("Z");
+        assertScoreForGuess(score, INCORRECT);
+    }
+    @Test
+    public void oneCorrectLetter() {
+        var word = new Word("A");
         var score = word.guess("A");
-        assertScoreForLetter(score,0,Letter.CORRECT);
+        assertScoreForGuess(score, CORRECT);
     }
-    private void assertScoreForLetter(Score score,
-        int position,Letter expected  ){
-        assertThat(score.letter(position)).
-                isEqualTo(expected);
-    }
-    //@Test
-    void secondLetterWrongPosition()
+
+    @Test
+    void secondLetterWrongPosition() // test para una segunda letra
     {
         var word = new Word("AR");
         var score = word.guess("ZA");
-        assertScoreForLetter(score,1, PART_CORRECT);
+        assertScoreForGuess(score, INCORRECT,
+                Letter.PART_CORRECT);
+    }
+    @Test
+    void allScoreCombinations() {
+        var word = new Word("ARI");
+        var score = word.guess("ZAI");
+        assertScoreForGuess(score, INCORRECT, PART_CORRECT,
+                CORRECT);
+    }
+
+    private void assertScoreForGuess(Score score,Letter... expectedScores){
+        for(int position=0; position < expectedScores.length;position++){
+            Letter expected = expectedScores[position];
+            assertThat(score.letter(position)).isEqualTo(expected);
+        }
     }
 }

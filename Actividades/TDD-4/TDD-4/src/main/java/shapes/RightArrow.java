e9bf888c51d6d5b23df6fcd5c71c94b0dcb8bc90
NaoDekoNeko <ademarx17@gmail.com>
2023-05-13 17:00:50 -0500

    TDD4 terminada, agregada flecha derecha

--- Actividades/TDD-4/TDD-4/src/main/java/shapes/RightArrow.java
+++ Actividades/TDD-4/TDD-4/src/main/java/shapes/RightArrow.java
@@ -1,9 +1,16 @@
 package shapes;
 
 public class RightArrow implements Shape{
-
     @Override
     public void draw(Graphics g) {
-
+        g.drawText("\\\\");
+        g.drawText(" \\\\");
+        System.out.print("  ");
+        g.drawHorizontalLine(10);
+        //no se puede agregar la punta de esta forma pues la forma en la que
+        //se dibuja la linea horizontal hace que al terminar de dibujar,
+        //realice un salto de linea
+        g.drawText(" //");
+        g.drawText("//");
     }
 }

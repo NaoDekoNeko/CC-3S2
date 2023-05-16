b5fbb7808847878f15950b5a6b3788772f745f2f
NaoDekoNeko <ademarx17@gmail.com>
2023-05-15 21:31:35 -0500

    Terminado hasta la Pregunta 3

---
+++ ExamenParcial-3S2/UnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("566d90b7-1876-4f0f-bb1d-d9bbaedc2a76")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ExamenParcial-3S2/UnitTest1/Requisito3.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReSOSgame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class Requisito3
+    {
+        private Juego juego;
+        private Tablero tablero;
+        [TestInitialize]
+        public void Init()
+        {
+            tablero = new Tablero(3);
+            juego = new JuegoSimple(tablero);
+        }
+        [TestMethod]
+        public void NoWinner()
+        {
+            Assert.IsFalse(juego.HayGanador());
+        }
+        [TestMethod]
+        public void HorizontalWin()
+        {
+            juego.Jugar(1, 1, Tablero.Cell.S);
+            juego.Jugar(1, 2, Tablero.Cell.O);
+            juego.Jugar(1, 3, Tablero.Cell.S);
+            Assert.IsTrue(juego.PuntoHorizontal());
+        }
+        [TestMethod]
+        public void VerticalWin()
+        {
+            juego.Jugar(1, 1, Tablero.Cell.S);
+            juego.Jugar(2, 1, Tablero.Cell.O);
+            juego.Jugar(3, 1, Tablero.Cell.S);
+            Assert.IsTrue(juego.PuntoVertical());
+        }
+        [TestMethod]
+        public void LeftDiagWin()
+        {
+            juego.Jugar(1, 1, Tablero.Cell.S);
+            juego.Jugar(2, 2, Tablero.Cell.O);
+            juego.Jugar(3, 3, Tablero.Cell.S);
+            Assert.IsTrue(juego.PuntoDiagIzq());
+        }
+        [TestMethod]
+        public void RightDiagWin()
+        {
+            juego.Jugar(1, 3, Tablero.Cell.S);
+            juego.Jugar(2, 2, Tablero.Cell.O);
+            juego.Jugar(3, 1, Tablero.Cell.S);
+            Assert.IsTrue(juego.PuntoDiagDer());
+        }
+    }
+}
